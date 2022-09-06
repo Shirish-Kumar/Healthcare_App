aad83c03ff015cf3afd60e79e52b31ff0bae3cbe
shirish kumar <shirishkumar226@gmail.com>
2022-09-06 05:33:44 +0530

    Added Project Birthday Card App
    Contains a design for a birthday card greeting
    • Designed in Android Studio
    • Added Image
    • Added Text

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+
+//fun loginValidateExistingUser(): Boolean {
+//    // Check if Email ID and Password available in database
+//    // If only Email ID matches return password Error on screen and return false
+//    // else return email ID error on screen and return false
+//    // If both match return true
+//    return true
+//}
+//
+//fun main() {
+//    loginValidateExistingUser()
+//}
+
+class Solution {
+    var count = 0
+    fun twoSum(nums: IntArray, target: Int) {
+        nums.forEachIndexed { index, i ->
+            println("$index, $i")
+        }
+    }
+}
